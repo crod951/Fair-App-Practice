4f67e581931b3380c1e9e246aa15977fed02d2e8
Chris Rodrigues <crod951@gmail.com>
2018-10-17 19:16:57 -0700

    Added image carousel for vehicle images

--- src/pages/vehicle-details/VehicleDetails.js
+++ src/pages/vehicle-details/VehicleDetails.js
@@ -2,8 +2,10 @@ import React, {Component} from "react"
 import {connect} from "react-redux"
 import {getVehicleDetails} from "./dux/actions"
 
+import CarouselCard from './components/carousel-card/CarouselCard';
 import DetailCard from './components/detail-card/DetailCard';
 import DetailContainer from './components/detail-container/DetailContainer';
+import ImageCarousel from './components/image-carousel/ImageCarousel';
 
 import StyledText from 'components/styled-text/StyledText';
 
@@ -16,6 +18,9 @@ class VehicleDetails extends Component {
     console.log(this.props.vehicleDetails.image_location_list)
     return (
       <DetailContainer>
+        <CarouselCard>
+        {this.props.vehicleDetails.image_location_list ? <ImageCarousel vehicleImages={this.props.vehicleDetails.image_location_list} /> : <div />}
+        </CarouselCard>
         <DetailCard>
           <StyledText fontSize="30px" fontWeight="100">
             {this.props.vehicleDetails.model_year} {this.props.vehicleDetails.make}
