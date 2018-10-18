d104587733de48476f5a71c3f674af27f5aa0e98
Chris Rodrigues <crod951@gmail.com>
2018-10-17 19:58:35 -0700

    Changed image carousel to be on same card as vehicle details inside its own container. instead of separate.

--- src/pages/vehicle-details/components/carousel-container/CarouselContainer.js
+++ src/pages/vehicle-details/components/carousel-container/CarouselContainer.js
@@ -1,17 +1,12 @@
 import styled from 'styled-components';
 
-const CarouselCard = styled.div`
-  margin: 20px;
+const CarouselContainer = styled.div`
   width: 100%;
-  box-shadow: -1px 3px 7px 0px rgba(0, 0, 0, 0.15);
-  position: relative;
-  z-index: 100;
-  border-radius: 10px;
-
+  margin-bottom: 20px;
   transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
 
   height: ${props => props.loading ? '0' : '100%'}
   opacity: ${props => props.loading ? '0' : '1'}
 `;
 
-export default CarouselCard
+export default CarouselContainer
